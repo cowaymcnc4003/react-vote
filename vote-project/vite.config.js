371fa97f0ef0d84b5385dfeaba05317d5eba0762
mhlee <mcnccoway4003@gmail.com>
2024-10-04 15:18:22 +0900

    테스트 수정

--- vote-project/vite.config.js
+++ vote-project/vite.config.js
@@ -4,20 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-vote/',
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://mcnccoway.asuscomm.com:9000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+    host: '0.0.0.0',
+    port: 9780
   },
 })
-
-
-// server: {
-//   host: '0.0.0.0',
-//     port: 9780
-// },
